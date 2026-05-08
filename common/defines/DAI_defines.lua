0e5dea28538d5e6323cb198f7148b3288d939404
jameskicklighter <103899871+jameskicklighter@users.noreply.github.com>
2026-05-07 22:48:38 -0400

    Remove automatic plane designs granted from focuses for AI Ger and Sov. Add coal calculations to economy law consideration.

--- common/defines/DAI_defines.lua
+++ common/defines/DAI_defines.lua
@@ -10,6 +10,12 @@
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 1
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 1
 NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 1
+NDefines.NMilitary.LAND_EQUIPMENT_BASE_COST = 3
+NDefines.NMilitary.LAND_EQUIPMENT_RAMP_COST = 0
+NDefines.NMilitary.NAVAL_EQUIPMENT_BASE_COST = 1
+NDefines.NMilitary.AIR_EQUIPMENT_BASE_COST = 1
+NDefines.NMilitary.AIR_EQUIPMENT_RAMP_COST = 0
+NDefines.NMilitary.NAVAL_EQUIPMENT_RAMP_COST = 0
 
 
 -- =========== --
@@ -27,7 +33,7 @@ NDefines.NAI.RESEARCH_DAYS_BETWEEN_WEIGHT_UPDATE = 1
 NDefines.NAI.MAX_AHEAD_RESEARCH_PENALTY = 3                                    -- BASE GAME is 3
 -- NDefines.NAI.RESEARCH_NEW_WEIGHT_FACTOR = 0			                            -- Impact of previously unexplored tech weights. Higher means more random exploration.
 -- NDefines.NAI.RESEARCH_BONUS_FACTOR = 3                                              -- To which extent AI should care about bonuses to research
-NDefines.NAI.RESEARCH_AHEAD_OF_TIME_FACTOR = 1.5		                            -- To which extent AI should care about ahead of time penalties to research
+NDefines.NAI.RESEARCH_AHEAD_OF_TIME_FACTOR = 3		                            -- To which extent AI should care about ahead of time penalties to research
 -- NDefines.NAI.RESEARCH_BASE_DAYS = 60					                            -- AI adds a base number of days when weighting completion time for techs to ensure it doesn't only research quick techs
 NDefines.NAI.RESEARCH_MULTI_DOCTRINE_SCORE = 0
 NDefines.NAI.XP_RATIO_REQUIRED_TO_RESEARCH_WITH_XP = 3.0		-- AI will at least need this amount of xp compared to cost of a tech to reserch it with XP			
@@ -39,6 +45,9 @@ NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_NAVAL_DOCTRINE = 1   -- How quickly is desi
 NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_AIR_DOCTRINE = 1     -- How quickly is desire to unlock air doctrines accumulated?
 
 NDefines.NAI.DESIRE_USE_XP_TO_UPDATE_LAND_TEMPLATE = 100.0 --2.0    -- How quickly is desire to update/create templates accumulated?
+NDefines.NAI.VARIANT_CREATION_XP_RESERVE_LAND = 10
+NDefines.NAI.VARIANT_CREATION_XP_RESERVE_NAVY = 30
+NDefines.NAI.VARIANT_CREATION_XP_RESERVE_AIR  = 30
 
 NDefines.NAI.DESIRE_USE_XP_TO_UPGRADE_LAND_EQUIPMENT = 50.0  -- How quickly is desire to update/create land equipment variants accumulated?
 NDefines.NAI.DESIRE_USE_XP_TO_UPGRADE_NAVAL_EQUIPMENT = 100.0 -- How quickly is desire to update/create naval equipment variants accumulated?
@@ -48,9 +57,11 @@ NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_ARMY_SPIRIT = 0.4    -- How quickly is desi
 NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_NAVY_SPIRIT = 0.4   -- How quickly is desire to unlock naval spirits accumulated?
 NDefines.NAI.DESIRE_USE_XP_TO_UNLOCK_AIR_SPIRIT = 0.4     -- How quickly is desire to unlock air spirits accumulated?
 
-NDefines.NAI.DEFAULT_MODULE_VARIANT_CREATION_XP_CUTOFF_LAND = 50 --50	-- Army XP needed before attempting to create a variant of a type that uses the tank designer (the tank designer DLC feature must be active).
-NDefines.NAI.DEFAULT_MODULE_VARIANT_CREATION_XP_CUTOFF_NAVY = 25 --50	-- Same as above but for the ship designer.
-NDefines.NAI.DEFAULT_MODULE_VARIANT_CREATION_XP_CUTOFF_AIR = 25 --25	-- Same as above but for the ship designer.
+NDefines.NAI.DEFAULT_LEGACY_VARIANT_CREATION_XP_CUTOFF_LAND = 20
+NDefines.NAI.DEFAULT_MODULE_VARIANT_CREATION_XP_CUTOFF_LAND = 10
+NDefines.NAI.DEFAULT_MODULE_VARIANT_CREATION_XP_CUTOFF_NAVY = 50
+NDefines.NAI.DEFAULT_MODULE_VARIANT_CREATION_XP_CUTOFF_AIR = 25
+NDefines.NAI.DEFAULT_LEGACY_VARIANT_CREATION_XP_CUTOFF_AIR = 20
 
 -- Buildings
 -- BUILDING_TARGETS_BUILDING_PRIORITIES = {				-- buildings in order of pirority when considering building targets strategies. First has the greatest priority, omitted has the lowest. NOTE: not all buildings are supported by building targets strategies.
